1d38a6dc81ba03c6ac2f3c8d785564730ea47d5b
Jarle Kramer <jarlekramer@gmail.com>
2023-12-12 22:32:20 +0100

    update

--- fmrs_model_derive/src/lib.rs
+++ fmrs_model_derive/src/lib.rs
@@ -3,7 +3,7 @@ use quote::quote;
 use syn;
 
 #[proc_macro_derive(FmrsModel)]
-pub fn fmrs_model_derive(input: TokenStream) -> TokenStream {
+pub fn fmrs_model_derive(input: TokenStream) -> TokenStream { 
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     impl_fmrs_model(&input)
@@ -11,9 +11,32 @@ pub fn fmrs_model_derive(input: TokenStream) -> TokenStream {
 
 fn impl_fmrs_model(input: &syn::DeriveInput) -> TokenStream {
     let name = &input.ident;
-    let data = &input.data;
+    
+    let data: &syn::Data = &input.data;
 
-    let mut tokens = quote!{};
+    let tokens = match *data {
+        syn::Data::Struct(ref data) => {
+            match data.fields {
+                syn::Fields::Named(ref fields) => {
+                    println!("Found named fields");
+
+                    let field_names = fields.named.iter().map(|f| {&f.ident});
+
+                    quote!{
+                        impl #name {
+                            pub fn new() -> Self {
+                                Self {
+                                    #(#field_names: 0.0),*
+                                }
+                            }
+                        }
+                    }
+                },
+                syn::Fields::Unnamed(_) | syn::Fields::Unit => unimplemented!(),
+            }
+        },
+        syn::Data::Enum(_) | syn::Data::Union(_) =>  unimplemented!(),
+    };
 
     tokens.into()
 }
