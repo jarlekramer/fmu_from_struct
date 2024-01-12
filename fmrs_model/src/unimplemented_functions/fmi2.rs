54ce35cd9473a6ef6e9a3bbb942c40774de4e1e0
Jarle Kramer <jarlekramer@gmail.com>
2024-01-12 07:21:08 +0100

    update

--- fmrs_model/src/unimplemented_functions/fmi2.rs
+++ fmrs_model/src/unimplemented_functions/fmi2.rs
@@ -9,6 +9,12 @@ pub extern "C" fn fmi2GetVersion() -> fmi2String {
     "2.0".as_ptr() as fmi2String
 }
 
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn fmi2GetTypesPlatform() -> fmi2String {
+    "default".as_ptr() as fmi2String
+}
+
 // ------------------------------- Model managment -------------------------------------------------
 #[no_mangle]
 #[allow(non_snake_case)]
@@ -19,6 +25,38 @@ pub extern "C" fn fmi2SetupExperiment(
     _start_time: f64,
     _stop_time_defined: bool,
     _stop_time: f64,
+) -> fmi2Status {
+    fmi2Status::fmi2OK
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn fmi2EnterInitializationMode(
+    _component: *mut ffi::c_void,
+) -> fmi2Status {
+    fmi2Status::fmi2OK
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn fmi2ExitInitializationMode(
+    _component: *mut ffi::c_void,
+) -> fmi2Status {
+    fmi2Status::fmi2OK
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn fmi2Terminate(
+    _component: *mut ffi::c_void,
+) -> fmi2Status {
+    fmi2Status::fmi2OK
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn fmi2Reset(
+    _component: *mut ffi::c_void,
 ) -> fmi2Status {
     fmi2Status::fmi2OK
 }
